38d4a5b359118a362bb4755fd1f5aa9e64aed089
Sanwarul <sanwarul.fuzzionit@gmail.com>
2021-02-13 13:02:45 +0600

    Add backend.php file for seperate route

---
+++ routes/backend.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\RoleController;
+use App\Http\Controllers\Backend\DashboardController;
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::resource('roles', RoleController::class);
