d26bdfd15b8198acb7023953aa9bde794910347b
muhammadsanwarulislam <muhammad.sanwarul94@gmail.com>
2026-02-17 14:21:20 +0600

    enhance registration process

---
+++ client/app/components/UI/Icons/DropDown.vue
@@ -0,0 +1,5 @@
+<template>
+    <svg class="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 20 20" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 8l4 4 4-4" />
+    </svg>
+</template>
